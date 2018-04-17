14ce7dbd83109af3483542e31169fa0feb816a7c
Evan Odell <evanodell91@gmail.com>
2018-04-17 00:27:35 -0400

    fix some test errors

--- tests/testthat/test-dates.R
+++ tests/testthat/test-dates.R
@@ -1,6 +1,8 @@
 context("test-dates.R")
 
 test_that("dates works", {
+  skip_on_cran()
+
   d <- rfg_date(date="2017-11-04")
   expect_true(tibble::is.tibble(d))
 })
--- tests/testthat/test-location.R
+++ tests/testthat/test-location.R
@@ -2,7 +2,7 @@ context("test-location.R")
 
 test_that("location works", {
   c <- rfg_location(lat=39, lng=-75)
-  expect_true(is.tibble(c))
+  expect_true(tibble::is.tibble(c))
   expect_true("NJ" %in% c$state)
   expect_length(c, 19)
 })
