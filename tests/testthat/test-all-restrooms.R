94be08d37a81f91d69cd2f2b8c19c509d16006d4
Evan Odell <evanodell91@gmail.com>
2018-12-03 19:37:23 +0000

    speeding up tests

--- tests/testthat/test-all-restrooms.R
+++ tests/testthat/test-all-restrooms.R
@@ -2,6 +2,9 @@ context("test-all-restrooms.R")
 
 test_that("all restrooms works", {
   skip_on_cran()
+  skip_on_travis() # takes a long time and is fine to test locally
+  skip_on_appveyor()
+
   all <- rfg_all_restrooms(accessible = TRUE, unisex = TRUE)
   expect_true(tibble::is.tibble(all))
 
