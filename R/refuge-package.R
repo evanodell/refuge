88679d3f81e92bf400e472f6577ccfd06a9ee488
Evan Odell <evanodell91@gmail.com>
2021-01-11 18:56:10 +0100

    fix some link issues

--- R/refuge-package.R
+++ R/refuge-package.R
@@ -2,14 +2,14 @@
 
 #' refuge
 #'
-#' @description \href{Refuge Restrooms}{https://www.refugerestrooms.org/}
+#' @description [Refuge Restrooms]{https://www.refugerestrooms.org/)
 #' "is a web application that seeks to provide safe restroom access for
 #' transgender, intersex, and gender nonconforming individuals." The web
 #' application includes a public API, which this package allows access to.
 #' The API does not require users to sign up or use a key. For further details
-#' on the API or the organisation behind it,
-#' see the \href{API docs}{https://www.refugerestrooms.org/api/docs/}, or the
-#' \href{Refuge Restrooms homepage}{https://www.refugerestrooms.org/}.
+#' on the API and the organisation behind it,
+#' see the [API docs](https://www.refugerestrooms.org/api/docs/), or the
+#' [Refuge Restrooms homepage](https://www.refugerestrooms.org/).
 #'
 #' @description The API does not require an API key or user registration.
 #' Requests are made over HTTPS, and returned in JSON format. All request
