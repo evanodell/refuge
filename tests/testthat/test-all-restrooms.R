e99da15b30f1d5dfd5d504e80458b863c7a70b35
Evan Odell <evanodell91@gmail.com>
2021-01-11 12:29:15 +0100

    removing dependencies, cutting down on test times

--- tests/testthat/test-all-restrooms.R
+++ tests/testthat/test-all-restrooms.R
@@ -1,13 +1,9 @@
 context("test-all-restrooms.R")
 
 test_that("all restrooms works", {
-  skip_on_cran()
-  skip_on_travis() # takes a long time and is fine to test locally
-  skip_on_appveyor()
+  skip_on_cran() # takes a long time and is fine to test locally
+  skip_on_ci()
 
   all <- rfg_all_restrooms(accessible = TRUE, unisex = TRUE)
   expect_true(tibble::is.tibble(all))
-
-  all2 <- rfg_all_restrooms(accessible = FALSE, unisex = FALSE)
-  expect_true(tibble::is.tibble(all2))
 })
